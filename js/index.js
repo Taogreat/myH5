5705884f65b17c161673c9ca08bca3ef5a0c3389
guotao <843618949@qq.com>
2020-10-10 09:58:06 +0800

    'loading'

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
-
-window.onload=function(){
-    animation()
-}
+// 页面动画
 function animation(){
 	$('#loading').fadeOut()
 	let length=$('.content').children().length
 	let i=0
 	let interval=setInterval(function(){
 		if(i<length){
-			console.log($('.content').children()[i],i,$('.content').children()[1])
+			// console.log($('.content').children()[i],i,$('.content').children()[1])
 			$($('.content').children()[i]).addClass('show')
 			i++
 		}else{
@@ -37,3 +34,48 @@ function animation(){
 	
 	})
 }
+
+// 页面资源请求完毕，执行动画
+// 方案一  不行
+// window.onload=function(){
+//     animation()
+// }
+
+// 方案二 不行
+// document.onreadystatechange=function(){
+// 	// console.log(document.readyState)
+// 	console.log('onreadystatechange')
+// 	if(document.readyState == "complete"){
+// 		animation()
+// 	}
+// }
+
+
+// 方案三 判断所有资源的onload  
+// 图片 img.onload  音视频 media.oncanplay
+
+$(function(){
+	let i=0
+	let imgArr=['bg.jpg','img01.jpg','img02.gif','img03.jpg']
+	let total=imgArr.length+2
+	imgArr.forEach(function(item,index){
+		let img=new Image()
+		img.src=`./img/${item}`
+		img.onload=function(){
+			if(++i/total==1){
+				animation()
+			}
+		}
+	})
+	$('#mp4').on('canplay',function(){
+		if(++i/total==1){
+			animation()
+		}
+	})
+	$('#music').on('canplay',function(){
+		if(++i/total==1){
+			animation()
+		}
+	})
+	
+})
